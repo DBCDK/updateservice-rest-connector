b41da5548560806f4f5bb31581e6cc2cbc0b9a91
Anders Thrane Michelsen <atm@dbc.dk>
2019-10-11 13:59:28 +0200

    Add license header
    Auditors: jbn, jbr

--- src/main/java/dk/dbc/updateservice/UpdateServiceDoubleRecordCheckConnectorException.java
+++ src/main/java/dk/dbc/updateservice/UpdateServiceDoubleRecordCheckConnectorException.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+ * See license text in LICENSE.txt or at https://opensource.dbc.dk/licenses/gpl-3.0/
+ */
+
 package dk.dbc.updateservice;
 
 public class UpdateServiceDoubleRecordCheckConnectorException extends Exception {
