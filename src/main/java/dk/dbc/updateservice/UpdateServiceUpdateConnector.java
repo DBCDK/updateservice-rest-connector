88511e11f6b6106bfe4fc515c56db4fb64d8616c
Jesper Bruun <jbr@dbc.dk>
2020-04-28 14:54:11 +0200

    Add rest connector for updateservice
    Auditors: atm, jbn, hwha

---
+++ src/main/java/dk/dbc/updateservice/UpdateServiceUpdateConnector.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GPLv3
+ * See license text in LICENSE.txt or at https://opensource.dbc.dk/licenses/gpl-3.0/
+ */
+
+package dk.dbc.updateservice;
+
+import dk.dbc.httpclient.FailSafeHttpClient;
+import dk.dbc.httpclient.HttpPost;
+import dk.dbc.httpclient.PathBuilder;
+import dk.dbc.invariant.InvariantUtil;
+import dk.dbc.jsonb.JSONBContext;
+import dk.dbc.jsonb.JSONBException;
+import dk.dbc.updateservice.dto.UpdateRecordResponseDTO;
+import dk.dbc.updateservice.dto.UpdateServiceRequestDTO;
+import dk.dbc.util.Stopwatch;
+import dk.dbc.dataio.commons.utils.lang.StringUtil;
+import java.io.InputStream;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.core.Response;
+import net.jodah.failsafe.RetryPolicy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UpdateServiceUpdateConnector {
+    JSONBContext jsonbContext = new JSONBContext();
+    public enum TimingLogLevel {
+        TRACE, DEBUG, INFO, WARN, ERROR
+    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UpdateServiceUpdateConnector.class);
+    private static final String PATH_UPDATESERVICE = "/api/v1/updateservice";
+
+    private static final RetryPolicy RETRY_POLICY = new RetryPolicy()
+            .retryOn(Collections.singletonList(ProcessingException.class))
+            .retryIf((Response response) -> response.getStatus() == 404)
+            .withDelay(10, TimeUnit.SECONDS)
+            .withMaxRetries(1);
+
+    private final FailSafeHttpClient failSafeHttpClient;
+    private final String baseUrl;
+    private final UpdateServiceUpdateConnector.LogLevelMethod logger;
+
+    /**
+     * Returns new instance with default retry policy
+     *
+     * @param httpClient web resources client
+     * @param baseUrl    base URL for update service endpoint
+     */
+    public UpdateServiceUpdateConnector(Client httpClient, String baseUrl) {
+        this(FailSafeHttpClient.create(httpClient, RETRY_POLICY), baseUrl, UpdateServiceUpdateConnector.TimingLogLevel.INFO);
+    }
+
+    /**
+     * Returns new instance with default retry policy
+     *
+     * @param httpClient web resources client
+     * @param baseUrl    base URL for update service endpoint
+     * @param level      timings log level
+     */
+    public UpdateServiceUpdateConnector(Client httpClient, String baseUrl, UpdateServiceUpdateConnector.TimingLogLevel level) {
+        this(FailSafeHttpClient.create(httpClient, RETRY_POLICY), baseUrl, level);
+    }
+
+    /**
+     * Returns new instance with custom retry policy
+     *
+     * @param failSafeHttpClient web resources client with custom retry policy
+     * @param baseUrl            base URL for update service endpoint
+     */
+    public UpdateServiceUpdateConnector(FailSafeHttpClient failSafeHttpClient, String baseUrl) {
+        this(failSafeHttpClient, baseUrl, UpdateServiceUpdateConnector.TimingLogLevel.INFO);
+    }
+
+    /**
+     * Returns new instance with custom retry policy
+     *
+     * @param failSafeHttpClient web resources client with custom retry policy
+     * @param baseUrl            base URL for update service endpoint
+     * @param level              timings log level
+     */
+    public UpdateServiceUpdateConnector(FailSafeHttpClient failSafeHttpClient, String baseUrl, UpdateServiceUpdateConnector.TimingLogLevel level) {
+        this.failSafeHttpClient = InvariantUtil.checkNotNullOrThrow(
+                failSafeHttpClient, "failSafeHttpClient");
+        this.baseUrl = InvariantUtil.checkNotNullNotEmptyOrThrow(
+                baseUrl, "baseUrl");
+        switch (level) {
+            case TRACE:
+                logger = LOGGER::trace;
+                break;
+            case DEBUG:
+                logger = LOGGER::debug;
+                break;
+            case INFO:
+                logger = LOGGER::info;
+                break;
+            case WARN:
+                logger = LOGGER::warn;
+                break;
+            case ERROR:
+                logger = LOGGER::error;
+                break;
+            default:
+                logger = LOGGER::info;
+                break;
+        }
+    }
+
+    public UpdateRecordResponseDTO updateRecord(UpdateServiceRequestDTO updateServiceRequestDTO) throws UpdateServiceUpdateConnectorException, JSONBException {
+        final Stopwatch stopwatch = new Stopwatch();
+        try {
+            final InputStream responseStream = sendPostRequest(PATH_UPDATESERVICE, updateServiceRequestDTO, InputStream.class);
+            return jsonbContext.unmarshall(StringUtil.asString(responseStream), UpdateRecordResponseDTO.class);
+        } finally {
+            logger.log("doubleRecordCheck took {} milliseconds",
+                    stopwatch.getElapsedTime(TimeUnit.MILLISECONDS));
+        }
+    }
+
+    private <T> T sendPostRequest(String basePath, UpdateServiceRequestDTO updateServiceRequestDTO, Class<T> type)
+            throws UpdateServiceUpdateConnectorException, JSONBException {
+        InvariantUtil.checkNotNullOrThrow(updateServiceRequestDTO, "updateServiceRequestDTO");
+        final PathBuilder path = new PathBuilder(basePath);
+        final HttpPost post = new HttpPost(failSafeHttpClient)
+                .withBaseUrl(baseUrl)
+                .withData(jsonbContext.marshall(updateServiceRequestDTO), "application/json")
+                .withHeader("Accept", "application/json")
+                .withPathElements(path.build());
+
+        final Response response = post.execute();
+        assertResponseStatus(response, Response.Status.OK);
+        return readResponseEntity(response, type);
+    }
+
+    private <T> T readResponseEntity(Response response, Class<T> type)
+            throws UpdateServiceUpdateConnectorException {
+        final T entity = response.readEntity(type);
+        if (entity == null) {
+            throw new UpdateServiceUpdateConnectorException(
+                    String.format("Update returned with null-valued %s entity",
+                            type.getName()));
+        }
+        return entity;
+    }
+
+    private void assertResponseStatus(Response response, Response.Status expectedStatus)
+            throws UpdateServiceUpdateConnectorException {
+        final Response.Status actualStatus =
+                Response.Status.fromStatusCode(response.getStatus());
+        if (actualStatus != expectedStatus) {
+            throw new UpdateServiceUpdateConnectorException(
+                    String.format("Update returned with '%s' status code: %s",
+                            actualStatus,
+                    actualStatus.getStatusCode()));
+        }
+    }
+
+    public void close() {
+        failSafeHttpClient.getClient().close();
+    }
+
+    @FunctionalInterface
+    interface LogLevelMethod {
+        void log(String format, Object... objs);
+    }
+
+}
---
+++ src/test/java/dk/dbc/updateservice/UpdateServiceUpdateConnectorTest.java
@@ -0,0 +1,163 @@
+package dk.dbc.updateservice;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import dk.dbc.httpclient.HttpClient;
+import dk.dbc.jsonb.JSONBException;
+import dk.dbc.updateservice.dto.AuthenticationDTO;
+import dk.dbc.updateservice.dto.BibliographicRecordDTO;
+import dk.dbc.updateservice.dto.MessageEntryDTO;
+import dk.dbc.updateservice.dto.RecordDataDTO;
+import dk.dbc.updateservice.dto.TypeEnumDTO;
+import dk.dbc.updateservice.dto.UpdateRecordResponseDTO;
+import dk.dbc.updateservice.dto.UpdateServiceRequestDTO;
+import dk.dbc.updateservice.dto.UpdateStatusEnumDTO;
+import java.util.Arrays;
+import java.util.List;
+import javax.ws.rs.client.Client;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.hamcrest.core.Is.is;
+
+public class UpdateServiceUpdateConnectorTest {
+    private static WireMockServer wireMockServer;
+    private static String wireMockHost;
+    static UpdateServiceUpdateConnector connector;
+
+    final static Client CLIENT = HttpClient.newClient(new ClientConfig()
+            .register(new JacksonFeature()));
+
+    @BeforeAll
+    static void startWireMockServer() {
+        wireMockServer = new WireMockServer(options().dynamicPort()
+                .dynamicHttpsPort());
+        wireMockServer.start();
+        wireMockHost = "http://localhost:" + wireMockServer.port();
+        configureFor("localhost", wireMockServer.port());
+    }
+
+    @BeforeAll
+    static void setConnector() {
+        connector = new UpdateServiceUpdateConnector(CLIENT, wireMockHost, UpdateServiceUpdateConnector.TimingLogLevel.INFO);
+    }
+
+    @AfterAll
+    static void stopWiremockServer() { wireMockServer.stop(); }
+
+    @Test
+    void checkThatConnectorWorksWithDTOS() throws JSONBException, UpdateServiceUpdateConnectorException {
+        final UpdateServiceRequestDTO  updateServiceRequestDTO = getExampleRequest();
+
+        final UpdateRecordResponseDTO expectedResponse = new UpdateRecordResponseDTO();
+        expectedResponse.setUpdateStatusEnumDTO(UpdateStatusEnumDTO.OK);
+
+        UpdateRecordResponseDTO actualRespons = connector.updateRecord(updateServiceRequestDTO);
+
+        assertThat("Update returns OK", actualRespons, is(expectedResponse));
+    }
+
+    @Test
+    void checkThatConnectorReturnsProperAuthFailure() throws JSONBException, UpdateServiceUpdateConnectorException {
+        UpdateServiceRequestDTO updateServiceRequestDTO = getExampleRequest();
+        updateServiceRequestDTO.getAuthenticationDTO().setGroupId("");
+
+        final UpdateRecordResponseDTO expectedResponse = new UpdateRecordResponseDTO();
+        expectedResponse.setUpdateStatusEnumDTO(UpdateStatusEnumDTO.FAILED);
+        MessageEntryDTO messageEntryDTO = new MessageEntryDTO();
+        messageEntryDTO.setMessage("Webservice RESPONDS with content: Agency not found");
+        messageEntryDTO.setType(TypeEnumDTO.FATAL);
+        expectedResponse.addMessageEntryDtos(Arrays.asList(messageEntryDTO));
+
+        UpdateRecordResponseDTO actualRespons = connector.updateRecord(updateServiceRequestDTO);
+
+        assertThat("Update returns Failure, with proper message", actualRespons, is(expectedResponse));
+    }
+
+    private UpdateServiceRequestDTO getExampleRequest() {
+        UpdateServiceRequestDTO updateServiceRequestDTO = new UpdateServiceRequestDTO();
+
+        AuthenticationDTO authenticationDTO = new AuthenticationDTO();
+        authenticationDTO.setGroupId("010100");
+        authenticationDTO.setPassword("");
+        authenticationDTO.setUserId("");
+
+        updateServiceRequestDTO.setAuthenticationDTO(authenticationDTO);
+        updateServiceRequestDTO.setSchemaName("dbcautoritet");
+        updateServiceRequestDTO.setTrackingId("update-warmup");
+
+        BibliographicRecordDTO bibliographicRecordDTO = new BibliographicRecordDTO();
+        bibliographicRecordDTO.setRecordSchema("info:lc/xmlns/marcxchange-v1");
+        bibliographicRecordDTO.setRecordPacking("xml");
+
+        RecordDataDTO recordDataDTO = new RecordDataDTO();
+        List<Object> content = Arrays.asList("<record xmlns=\"info:lc/xmlns/marcxchange-v1\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.loc.gov/standards/iso25577/marcxchange-1-1.xsd\">\n" +
+                "                            <leader>dbfhfgh2</leader>\n" +
+                "                            <datafield ind1=\"0\" ind2=\"0\" tag=\"001\">\n" +
+                "                                <subfield code=\"a\">68693268</subfield>\n" +
+                "                                <subfield code=\"b\">870979</subfield>\n" +
+                "                                <subfield code=\"c\">20181108150337</subfield>\n" +
+                "                                <subfield code=\"d\">20131129</subfield>\n" +
+                "                                <subfield code=\"f\">a</subfield>\n" +
+                "                                <subfield code=\"t\">faust</subfield>\n" +
+                "                            </datafield>\n" +
+                "                            <datafield ind1=\"0\" ind2=\"0\" tag=\"004\">\n" +
+                "                                <subfield code=\"r\">n</subfield>\n" +
+                "                                <subfield code=\"a\">e</subfield>\n" +
+                "                                <subfield code=\"x\">n</subfield>\n" +
+                "                            </datafield>\n" +
+                "                            <datafield ind1=\"0\" ind2=\"0\" tag=\"008\">\n" +
+                "                                <subfield code=\"t\">h</subfield>\n" +
+                "                                <subfield code=\"v\">9</subfield>\n" +
+                "                            </datafield>\n" +
+                "                            <datafield ind1=\"0\" ind2=\"0\" tag=\"025\">\n" +
+                "                                <subfield code=\"a\">5237167</subfield>\n" +
+                "                                <subfield code=\"2\">viaf</subfield>\n" +
+                "                                <subfield code=\"&amp;\">VIAF</subfield>\n" +
+                "                            </datafield>\n" +
+                "                            <datafield ind1=\"0\" ind2=\"0\" tag=\"025\">\n" +
+                "                                <subfield code=\"a\">0000000013134949</subfield>\n" +
+                "                                <subfield code=\"2\">isni</subfield>\n" +
+                "                                <subfield code=\"&amp;\">VIAF</subfield>\n" +
+                "                            </datafield>\n" +
+                "                            <datafield ind1=\"0\" ind2=\"0\" tag=\"040\">\n" +
+                "                                <subfield code=\"a\">DBC</subfield>\n" +
+                "                                <subfield code=\"b\">dan</subfield>\n" +
+                "                            </datafield>\n" +
+                "                            <datafield ind1=\"0\" ind2=\"0\" tag=\"043\">\n" +
+                "                                <subfield code=\"c\">dk</subfield>\n" +
+                "                                <subfield code=\"&amp;\">VIAF</subfield>\n" +
+                "                            </datafield>\n" +
+                "                            <datafield ind1=\"0\" ind2=\"0\" tag=\"100\">\n" +
+                "                                <subfield code=\"a\">Meilby</subfield>\n" +
+                "                                <subfield code=\"h\">Mogens</subfield>\n" +
+                "                            </datafield>\n" +
+                "                            <datafield ind1=\"0\" ind2=\"0\" tag=\"375\">\n" +
+                "                                <subfield code=\"a\">1</subfield>\n" +
+                "                                <subfield code=\"2\">iso5218</subfield>\n" +
+                "                                <subfield code=\"&amp;\">VIAF</subfield>\n" +
+                "                            </datafield>\n" +
+                "                            <datafield ind1=\"0\" ind2=\"0\" tag=\"d08\">\n" +
+                "                                <subfield code=\"o\">autogenereret</subfield>\n" +
+                "                            </datafield>\n" +
+                "                            <datafield ind1=\"0\" ind2=\"0\" tag=\"xyz\">\n" +
+                "                                <subfield code=\"u\">MEILBYMOGENS</subfield>\n" +
+                "                            </datafield>\n" +
+                "                            <datafield ind1=\"0\" ind2=\"0\" tag=\"z98\">\n" +
+                "                                <subfield code=\"a\">Minus korrekturprint</subfield>\n" +
+                "                            </datafield>\n" +
+                "                            <datafield ind1=\"0\" ind2=\"0\" tag=\"z99\">\n" +
+                "                                <subfield code=\"a\">VIAF</subfield>\n" +
+                "                            </datafield>\n" +
+                "                        </record>");
+        recordDataDTO.setContent(content);
+        bibliographicRecordDTO.setRecordDataDTO(recordDataDTO);
+        updateServiceRequestDTO.setBibliographicRecordDTO(bibliographicRecordDTO);
+        return updateServiceRequestDTO;
+    }
+}
